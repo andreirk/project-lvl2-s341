bffd7b239bd79405f3beb3cb9f722dea1535964a
andreyirk <sokolik_85@list.ru>
2018-10-11 08:24:41 -0400

    add files parsing

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env nodejs
-
+import program from 'commander';
 import gendiff from '../lib/gendiff';
 
 const args = process.argv;
 
-gendiff(args);
+program
+  .version('0.1.0')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format', '[type]  Output format')
+  .action((firstConfig, secondConfig) =>
+    console.log(gendiff(firstConfig, secondConfig)));
+
+program.parse(args);
 
--- src/lib/gendiff.js
+++ src/lib/gendiff.js
@@ -1,12 +1,18 @@
-import program from 'commander';
+import fs from 'fs';
+import path from 'path';
 
+export const compare = (file1, file2) => {
+  console.log('files', { file1, file2 });
+  return 'difference';
+};
 
-export default (args) => {
-  program
-    .version('0.1.0')
-    .arguments('<firstConfig> <secondConfig>')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format', '[type]  Output format')
-    .parse(args);
+export default (pathToFile1, pathToFile2) => {
+  const rawData1 = fs.readFileSync(path.resolve(pathToFile1), 'utf-8');
+  const rawData2 = fs.readFileSync(path.resolve(pathToFile2), 'utf-8');
+  const data1 = JSON.parse(rawData1);
+  const data2 = JSON.parse(rawData2);
+
+  const diff = compare(data1, data2);
+  return diff;
 };
 
