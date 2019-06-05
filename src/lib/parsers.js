4438987748636006e06b384887c7723c2976e6ce
andreyirk <sokolik_85@list.ru>
2019-06-05 09:28:11 +0300

    add ini parser

--- src/lib/parsers.js
+++ src/lib/parsers.js
@@ -1,4 +1,6 @@
 const yaml = require('js-yaml');
+const ini = require('ini');
+
 
 function parseYaml(file) {
   let doc;
@@ -11,9 +13,21 @@ function parseYaml(file) {
   return doc;
 }
 
+function parseIni(file) {
+  let doc;
+  try {
+    doc = ini.parse(file);
+    return doc;
+  } catch (e) {
+    console.log('error in parsers.js, parseIni:', e);
+  }
+  return doc;
+}
+
 const mapping = {
   '.json': file => JSON.parse(file),
   '.yml': file => parseYaml(file),
+  '.ini': file => parseIni(file),
 };
 
 const parse = (file, type) => {
